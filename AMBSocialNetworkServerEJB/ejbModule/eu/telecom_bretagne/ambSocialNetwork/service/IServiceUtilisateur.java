7d0a9e3721d7246305c5ff1332ebf9f63a576ddd
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-01-17 15:00:07 +0100

    Ébauche du service utilisateur.

---
+++ AMBSocialNetworkServerEJB/ejbModule/eu/telecom_bretagne/ambSocialNetwork/service/IServiceUtilisateur.java
@@ -0,0 +1,28 @@
+package eu.telecom_bretagne.ambSocialNetwork.service;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import eu.telecom_bretagne.ambSocialNetwork.data.model.Utilisateur;
+
+@Remote
+public interface IServiceUtilisateur
+{
+  //-----------------------------------------------------------------------------
+  public Utilisateur       nouvelUtilisateur(String nom, String prenom,
+                                             String email, String motDePasse,
+                                             String  urlAvatar, String description,
+                                             boolean partagePosition, boolean partagePositionPublic,
+                                             float latitude, float longitude, int cap, int vitesse);
+  public Utilisateur       nouvelUtilisateur(String nom, String prenom,
+                                             String email, String motDePasse,
+                                             String description);
+  public Utilisateur       authentification(String email, String motDePasse);
+  public Utilisateur       getUtilisateur(int id);
+  public void              effaceUtilisateur(int id);
+  public List<Utilisateur> listeDesUtilisateurs();
+  public List<Utilisateur> listeDeMesAmis(int id);
+  public List<Utilisateur> listeDeUtilisateursDontJeSuisAmi(int id);
+  //-----------------------------------------------------------------------------
+}
---
+++ AMBSocialNetworkServerEJB/ejbModule/eu/telecom_bretagne/ambSocialNetwork/service/ServiceUtilisateur.java
@@ -0,0 +1,101 @@
+package eu.telecom_bretagne.ambSocialNetwork.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import eu.telecom_bretagne.ambSocialNetwork.data.dao.UtilisateurDAO;
+import eu.telecom_bretagne.ambSocialNetwork.data.model.Utilisateur;
+
+/**
+ * Session Bean implementation class ServiceUtilisateur
+ */
+@Stateless
+@LocalBean
+public class ServiceUtilisateur implements IServiceUtilisateur
+{
+  //-----------------------------------------------------------------------------
+  @EJB UtilisateurDAO utilisateurDAO;
+  //-----------------------------------------------------------------------------
+  /**
+   * Default constructor.
+   */
+  public ServiceUtilisateur()
+  {
+    // TODO Auto-generated constructor stub
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public Utilisateur nouvelUtilisateur(String nom, String prenom,
+                                       String email, String motDePasse,
+                                       String urlAvatar, String description,
+                                       boolean partagePosition, boolean partagePositionPublic,
+                                       float latitude, float longitude, int cap, int vitesse)
+  {
+    Utilisateur utilisateur = new Utilisateur();
+    utilisateur.setNom(prenom);                                                   // non null
+    utilisateur.setPrenom(prenom!=null?(prenom.equals("")?null:prenom):null);
+    utilisateur.setEmail(email);                                                  // non null
+    utilisateur.setMotDePasse(motDePasse);                                        // non null
+    utilisateur.setUrlAvatar(urlAvatar!=null?(urlAvatar.equals("")?null:urlAvatar):null);
+    utilisateur.setDescription(description!=null?(description.equals("")?null:description):null);
+    utilisateur.setPartagePosition(partagePosition);
+    utilisateur.setPartagePositionPublic(partagePositionPublic);
+    utilisateur.setLatitude(latitude);
+    utilisateur.setLongitude(longitude);
+    utilisateur.setCap(cap);
+    utilisateur.setVitesse(vitesse);
+    
+    return utilisateur;
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public Utilisateur nouvelUtilisateur(String nom, String prenom,
+                                       String email, String motDePasse,
+                                       String description)
+  {
+    // TODO Auto-generated method stub
+    return nouvelUtilisateur(nom, prenom, email, motDePasse, null, description, false, false, -1, -1, -1, -1);
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public Utilisateur authentification(String email, String motDePasse)
+  {
+    return utilisateurDAO.findByEmailAndPassword(email, motDePasse);
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public Utilisateur getUtilisateur(int id)
+  {
+    return utilisateurDAO.findById(id);
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public void effaceUtilisateur(int id)
+  {
+    // TODO MessageDAO à ecrire afin d'effacer les messages (si implémenté) et les commentaires associés.
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public List<Utilisateur> listeDesUtilisateurs()
+  {
+    return utilisateurDAO.findAll();
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public List<Utilisateur> listeDeMesAmis(int id)
+  {
+    Utilisateur utilisateur = utilisateurDAO.findById(id);
+    return utilisateur.getUtilisateurs2();
+  }
+  //-----------------------------------------------------------------------------
+  @Override
+  public List<Utilisateur> listeDeUtilisateursDontJeSuisAmi(int id)
+  {
+    Utilisateur utilisateur = utilisateurDAO.findById(id);
+    return utilisateur.getUtilisateurs1();
+  }
+  //-----------------------------------------------------------------------------
+}
