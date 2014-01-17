94da2181bc27d004fad21ca90d773a7d2e0a538e
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-01-17 13:49:22 +0100

    Méthode "findByEmailAndPassword" pour l'authentification.

--- AMBSocialNetworkServerEJB/ejbModule/eu/telecom_bretagne/ambSocialNetwork/data/dao/UtilisateurDAO.java
+++ AMBSocialNetworkServerEJB/ejbModule/eu/telecom_bretagne/ambSocialNetwork/data/dao/UtilisateurDAO.java
@@ -48,6 +48,18 @@ public class UtilisateurDAO
     return entityManager.find(Utilisateur.class, id);
   }
   //----------------------------------------------------------------------------
+  public Utilisateur findByEmailAndPassword(String email, String motDePasse)
+  {
+    Query query = entityManager.createQuery("select utilisateur from Utilisateur utilisateur where utilisateur.email = :email and utilisateur.motDePasse = :motDePasse");
+    query.setParameter("email",      email);
+    query.setParameter("motDePasse", motDePasse);
+    @SuppressWarnings("rawtypes")
+    List l = query.getResultList();
+    if(l.size() == 0)
+      return null;
+    return (Utilisateur) l.get(0);
+  }
+  //----------------------------------------------------------------------------
   @SuppressWarnings({ "rawtypes", "unchecked" })
   public List<Utilisateur> findAll()
   {
