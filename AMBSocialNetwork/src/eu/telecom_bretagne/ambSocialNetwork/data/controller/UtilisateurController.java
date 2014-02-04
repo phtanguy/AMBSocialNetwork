b9399b3e529b5d5478d011bd20d23554f184b0e9
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-02-04 13:45:49 +0100

    Factorisation dans la classe COntroller : suite.

--- AMBSocialNetwork/src/eu/telecom_bretagne/ambSocialNetwork/data/controller/UtilisateurController.java
+++ AMBSocialNetwork/src/eu/telecom_bretagne/ambSocialNetwork/data/controller/UtilisateurController.java
@@ -1,26 +1,15 @@
 package eu.telecom_bretagne.ambSocialNetwork.data.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import android.util.Log;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.telecom_bretagne.ambSocialNetwork.data.model.Utilisateur;
 import eu.telecom_bretagne.ambSocialNetwork.data.model.UtilisateursList;
@@ -60,50 +49,16 @@ public class UtilisateurController extends Controller
   //-----------------------------------------------------------------------------
   public Utilisateur authentification(String email, String password) throws ClientProtocolException, IOException
   {
-    /*
-     * Plusieurs choses à faire :
-     *   - Préparer une requête POST de type formulaire (la
-     *     requête est l'équivalent d'un formulaire Web)
-     *       . Définir l'URL
-     *       . Encapsuler les paramètres 
-     *   - Envoyer la requête sur le serveur
-     *   - Récupérer le résultat (données au format JSON
-     *     représentant une utilisateur)
-     *   - Convertir les données dans un format objet (classe
-     *     utilisateur).
-     *   
-     */
+    Map<String,String> formValues = new HashMap<String, String>();
+    formValues.put("email",        email);
+    formValues.put("mot_de_passe", password);
     
-    // Réquête POST qui envrerra des données de type formulaire
-    
-    HttpPost post = new HttpPost(URL_UTILISATEUR + "/authentification");
-    post.setHeader("Content-Type",
-                   "application/x-www-form-urlencoded;charset=UTF-8");
-    
-    // Encapsulation des paramètres : le formulaire possède 2 paramètres
-    // (email et mot_de_passe). Attention c'est ici passé en clair mais
-    // c'est peu important dans notre contexte.
-    // Chaque paramètre est représenté dans une instance de NameValuePair
-    // (interface) qui contient un couple (nom du param, valeur). Chaque
-    // couple est ajouté dans une liste (List<NameValuePair>) qui représente
-    // l'ensemble des valeurs.
-    // Ces valeur sont ensuite encodées avant d'être incorporées au POST. 
-    
-    List<NameValuePair> values = new ArrayList<NameValuePair>();
-    values.add(new BasicNameValuePair("email",        email));
-    values.add(new BasicNameValuePair("mot_de_passe", password));
-    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(values);
-    post.setEntity(entity);
+    String jsonUtilisateurData = downloadContent(URL_UTILISATEUR + "/authentification", formValues);
 
-    // Envoi de la requête et récupération de la réponse.
-    
-    HttpResponse response = client.execute(post);
-    
     // Décodage de la réponse.
     Utilisateur utilisateur = null;
-    if(response != null)
+    if(jsonUtilisateurData != null)
     {
-      String jsonUtilisateurData =  EntityUtils.toString(response.getEntity(), "UTF-8");
       JsonParser   jParser  = jFactory.createParser(jsonUtilisateurData);
       try
       {
